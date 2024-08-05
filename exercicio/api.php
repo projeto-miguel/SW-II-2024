8f62fb4712089b751e100e1763814dc86251aab0
projeto-miguel <miguel2020carneiro@gmail.com>
2024-08-05 10:55:08 -0300

    commit

--- exercicio/api.php
+++ exercicio/api.php
@@ -5,4 +5,41 @@ include './conn.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-echo json_encode($method);
+$uri = $_SERVER['REQUEST_URI'];
+
+$path = parse_url($uri,PHP_URL_PATH);
+
+$path = trim($path,'/');
+
+$exploded = explode('/',$path);
+$url_parts = array();
+foreach($exploded as $part){
+    array_push($url_parts, $part ?? '');
+}
+$response = [
+    'method' => $method,
+    'url1' => $url_parts[1] ?? '',
+    'url2' => $url_parts[2] ?? '',
+    'url3' => $url_parts[3] ?? '',
+    'url4' => $url_parts[4] ?? ''
+];
+switch ($method) {
+    case 'GET':
+        //aaaaa
+        break;
+    case 'POST':
+        break;
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+    default:
+        echo json_encode([
+            'message' => 'method not allowed'
+        ]);
+        break;
+}
+
+
+
+// echo json_encode($response);
